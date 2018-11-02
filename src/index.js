d0ecc237127998dd054ceefe0c06e5fe7ef85c9e
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-02 11:34:07 -0500

    worked further into the exercise by installing jquery. Also worked on require and import methods.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 "use strict";
+const $ = require('jquery');
+// import mode:
+// import {niceMessage} from "./say-hello";
+// niceMessage();
+
+// require mode:
+const niceMessage = require('./say-hello');
 
 const sayHello = () => console.log('hello');
 
 sayHello();
+
+niceMessage();
+
+$("h3").html('Have a great day!');
+
+
+
---
+++ src/say-hello.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+// import mode:
+// export const niceMessage = () => console.log('Please have a nice day today!');
+
+
+// require mode:
+const niceMessage = () => console.log('Please have a nice day today!');
+
+module.exports = niceMessage;
